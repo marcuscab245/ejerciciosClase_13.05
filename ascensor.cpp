47413af3929c53354b1d56dddeef98dc6217e0c8
marcuscab245 <164245061+marcuscab245@users.noreply.github.com>
2024-05-15 11:09:44 -0600

    Add files via upload

---
+++ ascensor.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n = 0;
+
+    cout <<"A que nivel desea ir?\n";
+    cin >> n;
+    switch (n)
+    {
+        case 1:
+        cout <<"Usted se encuentra en el nivel 1.";
+        break;
+
+        case 2:
+        cout <<"Usted se encuentra en el nivel 2.";
+        break;
+
+        case 3:
+        cout <<"Usted se encuentra en el nivel 3.";
+        break;
+
+        case 4:
+        cout <<"Usted se encuentra en el nivel 4.";
+        break;
+
+        case 5:
+        cout <<"Usted se encuentra en el nivel 5.";
+        break;
+
+        case 6:
+        cout <<"Usted se encuentra en el nivel 6.";
+        break;
+
+        case 7:
+        cout <<"Usted se encuentra en el nivel 7.";
+        break;
+
+        case 8:
+        cout <<"Usted se encuentra en el nivel 8.";
+        break;
+
+        case 9:
+        cout <<"Usted se encuentra en el nivel 9.";
+        break;
+
+        case 10:
+        cout <<"Usted se encuentra en el nivel 10.";
+        break;
+
+        case 11:
+        cout <<"Usted se encuentra en el nivel 11.";
+        break;
+
+        case 12:
+        cout <<"Usted se encuentra en el nivel 12.";
+        break;
+
+        case 13:
+        cout <<"Usted se encuentra en el nivel 13.";
+        break;
+
+        case 14:
+        cout <<"Usted se encuentra en el nivel 14.";
+        break;
+
+        case 15:
+        cout <<"Usted se encuentra en el nivel 15.";
+        break;
+
+        case 16:
+        cout <<"Usted se encuentra en el nivel 16.";
+        break;
+
+        case 17:
+        cout <<"Usted se encuentra en el nivel 17.";
+        break;
+
+        case 18:
+        cout <<"Usted se encuentra en el nivel 18.";
+        break;
+
+        case 19:
+        cout <<"Usted se encuentra en el nivel 19.";
+        break;
+
+        case 20:
+        cout <<"Usted se encuentra en el nivel 20.";
+        break;
+
+        case 21:
+        cout <<"Usted se encuentra en el nivel 21.";
+        break;
+
+        case 22:
+        cout <<"Usted se encuentra en el nivel 22.";
+        break;
+
+        case 23:
+        cout <<"Usted se encuentra en el nivel 23.";
+        break;
+
+        case 24:
+        cout <<"Usted se encuentra en el nivel 24.";
+        break;
+
+        case 25:
+        cout <<"Ustedd se encuentra en el nivel 25";
+        break;
+    }
+    return 0;
+}
